9186009838485e93a2ad346610a3722bd1139d81
greglbd <greg@livebreathedigital.com>
2013-08-22 17:25:33 -0700

    set up compass with sass, added tabs

---
+++ assets/js/controllers.js
@@ -0,0 +1,10 @@
+function NavListCtrl($scope) {
+  $scope.nav = [
+    {"name": "HOME",
+     "link": "#home_tab"},
+    {"name": "ABOUT",
+     "link": "#about_tab"},
+    {"name": "MEDIA",
+     "link": "#media_tab"}
+  ];
+}
--- config.rb
+++ config.rb
@@ -1,11 +1,11 @@
 # Require any additional compass plugins here.
 
 # Set this to the root of your project when deployed:
-http_path = "/"
-css_dir = "stylesheets"
-sass_dir = "sass"
-images_dir = "images"
-javascripts_dir = "javascripts"
+http_path = "/ea/"
+css_dir = "assets/stylesheets"
+sass_dir = "assets/sass"
+images_dir = "assets/images"
+javascripts_dir = "assets/javascripts"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
